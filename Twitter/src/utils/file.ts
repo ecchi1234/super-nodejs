58777ef947d7550496fdf90adcfa64710a43d460
ecchi1234 <ngocchi.fit148.vnu@gmail.com>
2025-04-03 09:56:52 +0700

    fix bug wrong condition when check if exist uploads folder

--- Twitter/src/utils/file.ts
+++ Twitter/src/utils/file.ts
@@ -4,7 +4,7 @@ import path from 'path'
 
 export const initFolder = () => {
   const uploadFolderPath = path.resolve('uploads')
-  if (fs.existsSync(uploadFolderPath)) {
+  if (!fs.existsSync(uploadFolderPath)) {
     fs.mkdirSync(
       uploadFolderPath,
       { recursive: true } // mục đích là để tạo folder nested
