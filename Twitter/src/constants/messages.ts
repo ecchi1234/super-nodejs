1ecc37af6610f3f52ffcc34948dc51bd6bad6a81
ecchi1234 <ngocchi.fit148.vnu@gmail.com>
2025-04-18 16:26:23 +0700

    150: create create tweet validator

--- Twitter/src/constants/messages.ts
+++ Twitter/src/constants/messages.ts
@@ -65,3 +65,15 @@ export const USERS_MESSAGES = {
   GET_VIDEO_STATUS_SUCCESS: 'Get video status success',
   GMAIL_NOT_VERIFIED: 'Gmail not verified'
 } as const
+
+export const TWEETS_MESSAGES = {
+  INVALID_TYPE: 'Invalid type',
+  INVALID_AUDIENCE: 'Invalid audience',
+  PARENT_ID_MUST_BE_NULL: 'Parent id must be null',
+  PARENT_ID_MUST_BE_A_VALID_TWEET_ID: 'Parent id must be a valid tweet id',
+  CONTENT_MUST_BE_A_NON_EMPTY_STRING: 'Content must be a non-empty string',
+  CONTENT_MUST_BE_EMPTY_STRING: 'Content must be an empty string',
+  HASHTAGS_MUST_BE_ARRAY_OF_STRING: 'Hashtags must be an array of string',
+  MENTIONS_MUST_BE_ARRAY_OF_USER_ID: 'Mentions must be an array of user id',
+  MEDIAS_MUST_BE_ARRAY_OF_MEDIA_OBJECT: 'Medias must be an array of media object'
+} as const
