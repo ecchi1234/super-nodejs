365ef8a98e4cad4212b61eff67d7e887189c1d42
ecchi1234 <ngocchi.fit148.vnu@gmail.com>
2025-05-22 16:38:44 +0700

    190: fix bug about key aws

--- Twitter/src/middlewares/error.middlewares.ts
+++ Twitter/src/middlewares/error.middlewares.ts
@@ -4,16 +4,32 @@ import HTTP_STATUS from '~/constants/httpStatus'
 import { ErrorWithStatus } from '~/models/Errors'
 
 export const defaultErrorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
-  if (err instanceof ErrorWithStatus) {
-    return res.status(err.status).json(omit(err, ['status']))
+  try {
+    if (err instanceof ErrorWithStatus) {
+      return res.status(err.status).json(omit(err, ['status']))
+    }
+    // lưu ý: không được dùng Object.keys(err) vì nó sẽ không lấy được các key không enumerable
+    // thêm enumerable: true cho các key không enumerable để Object.keys(err) lấy được
+
+    const finalError: any = {}
+
+    Object.getOwnPropertyNames(err).forEach((key) => {
+      if (
+        !Object.getOwnPropertyDescriptor(err, key)?.configurable ||
+        !Object.getOwnPropertyDescriptor(err, key)?.writable
+      ) {
+        return
+      }
+      finalError[key] = err[key]
+    })
+    res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).json({
+      message: err.message,
+      errorInfo: omit(err, ['stack'])
+    })
+  } catch (error) {
+    res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).json({
+      message: 'Internal server error',
+      errorInfo: omit(error as any, ['stack'])
+    })
   }
-  // lưu ý: không được dùng Object.keys(err) vì nó sẽ không lấy được các key không enumerable
-  // thêm enumerable: true cho các key không enumerable để Object.keys(err) lấy được
-  Object.getOwnPropertyNames(err).forEach((key) => {
-    Object.defineProperty(err, key, { enumerable: true })
-  })
-  res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).json({
-    message: err.message,
-    errorInfo: omit(err, ['stack'])
-  })
 }
