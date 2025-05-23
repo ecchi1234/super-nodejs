34b3f8c628dfdebf98dbcb95f00988cf53245452
ecchi1234 <ngocchi.fit148.vnu@gmail.com>
2025-05-23 14:25:06 +0700

    192: upload file to s3

--- Twitter/src/utils/s3.ts
+++ Twitter/src/utils/s3.ts
@@ -1,5 +1,9 @@
-import { S3Client, ListBucketsCommand } from '@aws-sdk/client-s3'
+import { S3Client } from '@aws-sdk/client-s3'
 import { config } from 'dotenv'
+import { Upload } from '@aws-sdk/lib-storage'
+import fs from 'fs'
+import path from 'path'
+
 config()
 
 const client = new S3Client({
@@ -10,8 +14,32 @@ const client = new S3Client({
   }
 })
 
-const command = new ListBucketsCommand({})
+const file = fs.readFileSync(path.resolve('uploads/images/ffd9fe80e2cffcf948a929001.jpg'))
+const parallelUploads3 = new Upload({
+  client: client,
+  params: { Bucket: 'chi-twitter-clone-ap-southeast-1-2025', Key: 'anh1.jpg', Body: file, ContentType: 'image/jpeg' },
 
-client.send(command).then((data) => {
-  console.log(data)
+  // optional tags
+  tags: [
+    /*...*/
+  ],
+
+  // additional optional fields show default values below:
+
+  // (optional) concurrency configuration
+  queueSize: 4,
+
+  // (optional) size of each part, in bytes, at least 5MB
+  partSize: 1024 * 1024 * 5,
+
+  // (optional) when true, do not automatically call AbortMultipartUpload when
+  // a multipart upload fails to complete. You should then manually handle
+  // the leftover parts.
+  leavePartsOnError: false
 })
+
+parallelUploads3.on('httpUploadProgress', (progress) => {
+  console.log(progress)
+})
+
+parallelUploads3.done().then((res) => console.log(res))
